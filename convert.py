c5af3f041ba09d6f4d31d1d41c957dae917e17ab
KdeInit <162863344+KdeInit@users.noreply.github.com>
2024-07-04 20:04:50 -0400

    fix: correct filename extraction logic to handle filenames with periods

--- convert.py
+++ convert.py
@@ -6,60 +6,73 @@ import requests
 import yaml
 import ipaddress
 
+
 def read_yaml_from_url(url):
     response = requests.get(url)
     response.raise_for_status()
     yaml_data = yaml.safe_load(response.text)
     return yaml_data
 
+
 def read_list_from_url(url):
-    df = pd.read_csv(url, header=None, names=['pattern', 'address', 'other'], on_bad_lines='warn')
+    df = pd.read_csv(
+        url, header=None, names=["pattern", "address", "other"], on_bad_lines="warn"
+    )
     return df
+
 
 def is_ipv4_or_ipv6(address):
     try:
         ipaddress.IPv4Network(address)
-        return 'ipv4'
+        return "ipv4"
     except ValueError:
         try:
             ipaddress.IPv6Network(address)
-            return 'ipv6'
+            return "ipv6"
         except ValueError:
             return None
 
+
 def parse_and_convert_to_dataframe(link):
-    if link.endswith('.yaml') or link.endswith('.txt'):
+    if link.endswith(".yaml") or link.endswith(".txt"):
         try:
             yaml_data = read_yaml_from_url(link)
             rows = []
             if not isinstance(yaml_data, str):
-                items = yaml_data.get('payload', [])
+                items = yaml_data.get("payload", [])
             else:
                 lines = yaml_data.splitlines()
                 line_content = lines[0]
                 items = line_content.split()
             for item in items:
                 address = item.strip("'")
-                if ',' not in item:
+                if "," not in item:
                     if is_ipv4_or_ipv6(item):
-                        pattern = 'IP-CIDR'
+                        pattern = "IP-CIDR"
                     else:
-                        if address.startswith('+') or address.startswith('.'):
-                            pattern = 'DOMAIN-SUFFIX'
+                        if address.startswith("+") or address.startswith("."):
+                            pattern = "DOMAIN-SUFFIX"
                             address = address[1:]
-                            if address.startswith('.'):
+                            if address.startswith("."):
                                 address = address[1:]
                         else:
-                            pattern = 'DOMAIN'
+                            pattern = "DOMAIN"
                 else:
-                    pattern, address = item.split(',', 1)  
-                rows.append({'pattern': pattern.strip(), 'address': address.strip(), 'other': None})
-            df = pd.DataFrame(rows, columns=['pattern', 'address', 'other'])
+                    pattern, address = item.split(",", 1)
+                rows.append(
+                    {
+                        "pattern": pattern.strip(),
+                        "address": address.strip(),
+                        "other": None,
+                    }
+                )
+            df = pd.DataFrame(rows, columns=["pattern", "address", "other"])
         except:
             df = read_list_from_url(link)
     else:
         df = read_list_from_url(link)
     return df
+
 
 def sort_dict(obj):
     if isinstance(obj, dict):
@@ -71,52 +84,64 @@ def sort_dict(obj):
     else:
         return obj
 
+
 def parse_list_file(link, output_directory):
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = list(executor.map(parse_and_convert_to_dataframe, [link]))
         df = pd.concat(results, ignore_index=True)
-
-    df = df[~df['pattern'].str.contains('#')].reset_index(drop=True)
-
-    map_dict = {'DOMAIN-SUFFIX': 'domain_suffix', 'HOST-SUFFIX': 'domain_suffix', 'DOMAIN': 'domain', 'HOST': 'domain', 'host': 'domain',
-                'DOMAIN-KEYWORD':'domain_keyword', 'HOST-KEYWORD': 'domain_keyword', 'host-keyword': 'domain_keyword', 'IP-CIDR': 'ip_cidr',
-                'ip-cidr': 'ip_cidr', 'IP-CIDR6': 'ip_cidr', 
-                'IP6-CIDR': 'ip_cidr','SRC-IP-CIDR': 'source_ip_cidr', 'GEOIP': 'geoip', 'DST-PORT': 'port',
-                'SRC-PORT': 'source_port', "URL-REGEX": "domain_regex"}
-
-    df = df[df['pattern'].isin(map_dict.keys())].reset_index(drop=True)
-
+    df = df[~df["pattern"].str.contains("#")].reset_index(drop=True)
+    map_dict = {
+        "DOMAIN-SUFFIX": "domain_suffix",
+        "HOST-SUFFIX": "domain_suffix",
+        "DOMAIN": "domain",
+        "HOST": "domain",
+        "host": "domain",
+        "DOMAIN-KEYWORD": "domain_keyword",
+        "HOST-KEYWORD": "domain_keyword",
+        "host-keyword": "domain_keyword",
+        "IP-CIDR": "ip_cidr",
+        "ip-cidr": "ip_cidr",
+        "IP-CIDR6": "ip_cidr",
+        "IP6-CIDR": "ip_cidr",
+        "SRC-IP-CIDR": "source_ip_cidr",
+        "GEOIP": "geoip",
+        "DST-PORT": "port",
+        "SRC-PORT": "source_port",
+        "URL-REGEX": "domain_regex",
+    }
+    df = df[df["pattern"].isin(map_dict.keys())].reset_index(drop=True)
     df = df.drop_duplicates().reset_index(drop=True)
-    df['pattern'] = df['pattern'].replace(map_dict)
-
+    df["pattern"] = df["pattern"].replace(map_dict)
     os.makedirs(output_directory, exist_ok=True)
 
     result_rules = {"version": 1, "rules": []}
     domain_entries = []
-
-    for pattern, addresses in df.groupby('pattern')['address'].apply(list).to_dict().items():
-        if pattern == 'domain_suffix':
-            rule_entry = {pattern: ['.' + address.strip() for address in addresses]}
+    for pattern, addresses in (
+        df.groupby("pattern")["address"].apply(list).to_dict().items()
+    ):
+        if pattern == "domain_suffix":
+            rule_entry = {pattern: ["." + address.strip() for address in addresses]}
             result_rules["rules"].append(rule_entry)
             domain_entries.extend([address.strip() for address in addresses])
-        elif pattern == 'domain':
+        elif pattern == "domain":
             domain_entries.extend([address.strip() for address in addresses])
         else:
             rule_entry = {pattern: [address.strip() for address in addresses]}
             result_rules["rules"].append(rule_entry)
     domain_entries = list(set(domain_entries))
     if domain_entries:
-        result_rules["rules"].insert(0, {'domain': domain_entries})
-
-    file_name = os.path.join(output_directory, f"{os.path.basename(link).split('.')[0]}.json")
-    with open(file_name, 'w', encoding='utf-8') as output_file:
+        result_rules["rules"].insert(0, {"domain": domain_entries})
+    base_file_name = os.path.basename(link).rsplit(".", 1)[0]
+    file_name = os.path.join(output_directory, f"{base_file_name}.json")
+    with open(file_name, "w", encoding="utf-8") as output_file:
         json.dump(sort_dict(result_rules), output_file, ensure_ascii=False, indent=2)
 
     srs_path = file_name.replace(".json", ".srs")
     os.system(f"sing-box rule-set compile --output {srs_path} {file_name}")
     return file_name
 
-with open("../source.txt", 'r') as links_file:
+
+with open("../source.txt", "r") as links_file:
     links = links_file.read().splitlines()
 
 links = [l for l in links if l.strip() and not l.strip().startswith("#")]
